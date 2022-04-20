7a154e2562eb3fa568622ef372b4181b02d0f32b
Elayd <iamvine@mail.ru>
2022-04-20 15:02:06 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+console.log('arr'+ " - object");
+console.log(4 + "5");
+
+let incr = 10,
+    decr =10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5+2 == '3');
+
+const isChecked = true,
+      isClose = true;
+console.log(isChecked && isClose);
